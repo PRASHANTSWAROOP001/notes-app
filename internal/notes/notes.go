c0af21401ac897f284879a337def9036223c34dc
PRASHANTSWAROOP001 <mrprashantswaroop@gmail.com>
2025-11-05 20:10:25 +0530

    added notes has interfaces and structs.

---
+++ internal/notes/notes.go
@@ -0,0 +1,53 @@
+package notes
+
+import (
+	"context"
+	"time"
+)
+
+type Note struct {
+	ID         string    `json:"id"`
+	AuthorID   string    `json:"author_id"`
+	Title      string    `json:"title"`
+	Content    string    `json:"content"`
+	Public     bool      `json:"public"`
+	Slug       *string   `json:"slug,omitempty"`
+	SharedWith []string  `json:"shared_with,omitempty"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+}
+
+// this is for recieving values for admin and how much notes it created in list.
+type NoteSummary struct {
+	ID        string    `json:"id"`
+	AuthorID  string    `json:"author_id"`
+	Title     string    `json:"title"`
+	Public    bool      `json:"public"`
+	Slug      *string   `json:"slug,omitempty"`
+	CreatedAt time.Time `json:"created_at"`
+}
+// this is to be used by repository like must be implemented function handling database.
+type NotesRepository interface {
+	CreateNote(ctx context.Context, n *Note) (*Note, error)
+	//UpdateNote(ctx context.Context, n *Note) (*Note, error)
+	//DeleteNote(ctx context.Context, noteId, authorId string) error
+
+	//GetNoteByID(ctx context.Context, noteID, authorID string) (*Note, error)
+	GetNotesByAuthor(ctx context.Context, authorID string) ([]*NoteSummary, error)
+
+	//GetNoteBySlug(ctx context.Context, slug string) (*Note, error)
+	//AddEmailShare(ctx context.Context, noteId, emailId string) error
+	//RemoveEmailShare(ctx context.Context, noteId, emailId string) error
+
+}
+
+// this is to be implemented by services will be used via repos and handler.
+type NotesService interface {
+	CreateNote(ctx context.Context, note *Note) (*Note, error)
+	//UpdateNote(ctx context.Context, note *Note, userID string) (*Note, error)
+	//DeleteNote(ctx context.Context, noteID, userID string) error
+	GetUserNotes(ctx context.Context, userID string) ([]*NoteSummary, error)
+
+	//GetPublicNote(ctx context.Context, slug string) (*Note, error)
+	//ShareNoteViaEmail(ctx context.Context, noteID, ownerID, email string) error
+}
