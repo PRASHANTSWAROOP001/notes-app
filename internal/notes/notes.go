bd6e70169444a8dd68c9b7fab200d721eef16ba3
PRASHANTSWAROOP001 <mrprashantswaroop@gmail.com>
2025-11-06 12:42:54 +0530

    uncommented to implement service delete and view

--- internal/notes/notes.go
+++ internal/notes/notes.go
@@ -26,13 +26,14 @@ type NoteSummary struct {
 	Slug      *string   `json:"slug,omitempty"`
 	CreatedAt time.Time `json:"created_at"`
 }
+
 // this is to be used by repository like must be implemented function handling database.
 type NotesRepository interface {
 	CreateNote(ctx context.Context, n *Note) (*Note, error)
 	//UpdateNote(ctx context.Context, n *Note) (*Note, error)
-	//DeleteNote(ctx context.Context, noteId, authorId string) error
+	DeleteNote(ctx context.Context, noteId, authorId string) error
 
-	//GetNoteByID(ctx context.Context, noteID, authorID string) (*Note, error)
+	GetNoteByID(ctx context.Context, noteID, authorID string) (*Note, error)
 	GetNotesByAuthor(ctx context.Context, authorID string) ([]*NoteSummary, error)
 
 	//GetNoteBySlug(ctx context.Context, slug string) (*Note, error)
@@ -45,9 +46,9 @@ type NotesRepository interface {
 type NotesService interface {
 	CreateNote(ctx context.Context, note *Note) (*Note, error)
 	//UpdateNote(ctx context.Context, note *Note, userID string) (*Note, error)
-	//DeleteNote(ctx context.Context, noteID, userID string) error
+	DeleteNote(ctx context.Context, noteID, userID string) error
 	GetUserNotes(ctx context.Context, userID string) ([]*NoteSummary, error)
-
+	GetUserNote(ctx context.Context, noteID, userID string) (*Note, error)
 	//GetPublicNote(ctx context.Context, slug string) (*Note, error)
 	//ShareNoteViaEmail(ctx context.Context, noteID, ownerID, email string) error
 }
