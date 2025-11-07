8b799f66cad310b2787cb90141a178bdf083da4a
PRASHANTSWAROOP001 <mrprashantswaroop@gmail.com>
2025-11-07 22:05:52 +0530

    services to include all the methods

--- internal/notes/service.go
+++ internal/notes/service.go
@@ -2,6 +2,8 @@ package notes
 
 import (
 	"context"
+	//"crypto/sha1"
+	//"encoding/hex"
 	"fmt"
 	"strings"
 	"time"
@@ -49,11 +51,6 @@ func (s *service) CreateNote(ctx context.Context, n *Note) (*Note, error) {
 	n.CreatedAt = now
 	n.UpdatedAt = now
 
-	if n.Public {
-		slug := slugify(n.Title)
-		n.Slug = &slug
-	}
-
 	createdNote, err := s.repo.CreateNote(ctx, n)
 
 	if err != nil {
@@ -62,12 +59,6 @@ func (s *service) CreateNote(ctx context.Context, n *Note) (*Note, error) {
 
 	return createdNote, nil
 
-}
-
-func slugify(title string) string {
-	s := strings.ToLower(title)
-	s = strings.ReplaceAll(s, " ", "-")
-	return s
 }
 
 func (s *service) GetUserNotes(ctx context.Context, userID string) ([]*NoteSummary, error) {
@@ -109,4 +100,103 @@ func (s *service) DeleteNote(ctx context.Context, noteID, userID string) error {
 	}
 
 	return nil
+}
+
+func (s *service) UpdateNote(ctx context.Context, n *Note) (*NoteSummary, error) {
+
+	if n.AuthorID == "" {
+		return nil, fmt.Errorf("missing authour id")
+	}
+
+	if n.Content == "" {
+		return nil, fmt.Errorf("missing content")
+	}
+
+	if n.Title == "" {
+		return nil, fmt.Errorf("missing title")
+	}
+
+	noteSummary, err := s.repo.UpdateNote(ctx, n)
+
+	if err != nil {
+		return nil, err
+	}
+	return noteSummary, nil
+}
+
+func (s *service) ShareNoteViaEmail(ctx context.Context, notesId, ownerid, email string) error {
+
+	if ownerid == "" {
+		return fmt.Errorf("unauthrozied access attempt")
+	}
+
+	if email == "" {
+		return fmt.Errorf("empty email cant be provided")
+	}
+
+	if notesId == "" {
+		return fmt.Errorf("empty")
+	}
+
+	err := s.repo.AddEmailShare(ctx, notesId, ownerid, email)
+
+	if err != nil {
+		return fmt.Errorf("error %w", err)
+	}
+
+	return nil
+}
+
+func (s *service) RevokeEmailAccess(ctx context.Context, noteID, ownerID, email string) error {
+
+	if ownerID == "" {
+		return fmt.Errorf("unauthrozied access attempt")
+	}
+
+	if email == "" {
+		return fmt.Errorf("empty email cant be provided")
+	}
+
+	if noteID == "" {
+		return fmt.Errorf("empty")
+	}
+
+	err := s.repo.RemoveEmailShare(ctx, noteID, ownerID, email)
+
+	if err != nil {
+		return fmt.Errorf("error %w", err)
+	}
+
+	return nil
+}
+
+func (s *service) GetPublicNote(ctx context.Context, slug string, userId, emailId *string)(*Note, error){
+
+	note, err := s.repo.GetNoteBySlug(ctx,slug, userId, emailId)
+
+	if err != nil {
+		return nil, fmt.Errorf("error %w", err)
+	}
+
+	return note,nil
+}
+
+func slugify(title string) string {
+	s := strings.ToLower(title)
+	s = strings.ReplaceAll(s, " ", "-")
+	return s
+}
+
+// func slugifyWithHash(title, id string) string{
+// 	base := slugify(title)
+// 	h := sha1.New()
+// 	h.Write([]byte(id))
+// 	hash := hex.EncodeToString(h.Sum(nil))[:6]
+// 	return fmt.Sprintf("%s-%s",base,hash)
+// }
+
+func slugifyWithID(title, id string) string {
+	base := slugify(title)
+	shortID := id[:6]
+	return fmt.Sprintf("%s-%s", base, shortID)
 }
